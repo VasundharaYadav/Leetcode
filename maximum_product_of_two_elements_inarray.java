85b559146d9c4ccbf02f932a9b30c49247d3337b
Vasundhara Yadav <vasundharayadav222@gmail.com>
2025-02-11 11:11:58 +0530

    Add files via upload

---
+++ maximum_product_of_two_elements_inarray.java
@@ -0,0 +1,19 @@
+public class maximum_product_of_two_elements_inarray {
+        public int maxProduct(int[] nums) {
+            int smax = -1 ;
+            int max = -1 ;
+    
+            for(int i=0;i<nums.length;i++){
+                if(max<nums[i]){
+                    smax = max ;
+                    max = nums[i];
+                }else if(smax<nums[i]){
+                    smax = nums[i];
+                }
+            }
+    
+            int ans = (max-1) * (smax-1);
+            return ans;
+        }
+    } 
+
